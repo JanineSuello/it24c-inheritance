a496ae052da9e716744e112b6c2bfcc4d52b0e60
JanineSuello <janinegrasyanasuello@gmail.com>
2023-11-10 21:17:06 +0800

    first commit

---
+++ src/Siblings.java
@@ -0,0 +1,4 @@
+
+public class Siblings extends Siblings {
+    
+}
---
+++ src/family.java
@@ -0,0 +1,20 @@
+public class family {
+    
+    String name;
+    
+    family (String name){
+        this.name= name;
+    }
+    
+    void jasper(){
+        System.out.println(name + "is the eldest");
+    }
+    
+    void jonas(){
+        System.out.println(name + "is the 2nd born");
+    }
+    
+     void joshua(){
+        System.out.println(name + "is the youngest");
+    }
+}
